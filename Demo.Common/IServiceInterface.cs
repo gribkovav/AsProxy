0329f8c76313ccc70009030dc1940b6bbd7d5af4
gribkovav <gribkovav@gmail.com>
2019-01-12 00:10:51 +0300

    Demo init

---
+++ Demo.Common/IServiceInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Common
+{
+	interface IServiceInterface
+	{
+		SomeCompicatedModel GiveMeMyModel(int intVal, string stringVal, A aModel);
+	}
+}
---
+++ Demo.Common/SomeCompicatedModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Common
+{
+	public class SomeCompicatedModel
+	{
+		public int SomeInt { get; set; }
+
+		public object[] SomeArray { get; set; }
+
+		public string SomeString { get; set; }
+	}
+
+	public abstract class A
+	{
+		public virtual void WhoAmI()
+		{
+			Console.WriteLine("I'm A-class");
+		}
+	}
+
+	public  class B : A
+	{
+		public override void WhoAmI()
+		{
+			Console.WriteLine("I'm D-class");
+		}
+	}
+
+	public  class C : A
+	{
+		public override void WhoAmI()
+		{
+			Console.WriteLine("I'm C-class");
+		}
+	}
+}
